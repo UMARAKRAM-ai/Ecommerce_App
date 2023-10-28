69d1415e7385d84385f2c4a37564791964e80149
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-10-28 23:15:42 +0500

    Product Listing

---
+++ frontend/src/product.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/logo512.png",
+    description:
+      "Bluetooth technology lets you connect it with compatible device wirelessly High quality audio offers immersive listening experience built in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: "99.21",
+    countInStock: "10",
+    rating: "4.5",
+    numReviews: "22",
+  },
+  {
+    _id: "2",
+    name: "Sony WH-1000XM4 Wireless Noise-Canceling Headphones",
+    image: "https://example.com/headphones2.jpg",
+    description:
+      "Industry-leading noise cancellation for a quiet and immersive listening experience. Touch controls for easy playback and calls. Long battery life.",
+    brand: "Sony",
+    category: "Electronics",
+    price: "299.99",
+    countInStock: "15",
+    rating: "4.8",
+    numReviews: "32",
+  },
+  {
+    _id: "3",
+    name: "Samsung 55-Inch 4K Ultra HD Smart TV",
+    image: "https://example.com/smart-tv.jpg",
+    description:
+      "Stunning 4K Ultra HD picture quality. Built-in smart features for streaming your favorite content. Multiple HDMI and USB ports.",
+    brand: "Samsung",
+    category: "Electronics",
+    price: "549.99",
+    countInStock: "8",
+    rating: "4.6",
+    numReviews: "18",
+  },
+  {
+    _id: "4",
+    name: "Canon EOS Rebel T7 DSLR Camera",
+    image: "https://example.com/camera.jpg",
+    description:
+      "Capture high-quality photos and Full HD videos with this entry-level DSLR camera. Easy-to-use and great for beginners.",
+    brand: "Canon",
+    category: "Electronics",
+    price: "499.00",
+    countInStock: "12",
+    rating: "4.4",
+    numReviews: "26",
+  },
+  {
+    _id: "5",
+    name: "KitchenAid Artisan Stand Mixer",
+    image: "https://example.com/stand-mixer.jpg",
+    description:
+      "Powerful stand mixer for baking and cooking enthusiasts. Multiple attachments for versatile food preparation. Durable and stylish design.",
+    brand: "KitchenAid",
+    category: "Appliances",
+    price: "299.99",
+    countInStock: "7",
+    rating: "4.9",
+    numReviews: "38",
+  },
+  {
+    _id: "6",
+    name: "Fitbit Charge 4 Fitness Tracker",
+    image: "https://example.com/fitness-tracker.jpg",
+    description:
+      "Track your fitness and health stats with this advanced fitness tracker. GPS, heart rate monitoring, and sleep tracking features.",
+    brand: "Fitbit",
+    category: "Wearable Tech",
+    price: "129.95",
+    countInStock: "20",
+    rating: "4.7",
+    numReviews: "48",
+  },
+];
+
+export default products;
