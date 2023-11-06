4a22fa257814d36e00be794a48a9f5bbe26f099c
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-11-06 12:02:28 +0500

    New user Registration

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,30 @@
+import {useNavigate} from 'react-router-dom'
 import { Navbar, Nav, Container, Badge,NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/loginSlice";
 import logo from "../assets/styles/Logo.png";
 
 const Header = () => {
 
   const { cartItems } = useSelector((state) => state.cart);
-  
   const { userInfo } = useSelector((state) => state.login);
 
-  const logoutHandler = () => {
-    console.log('logout');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
