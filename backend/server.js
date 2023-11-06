f9285724b584417b785aaf51016cc6c17083b149
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-11-06 15:08:03 +0500

    Products Image added

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,10 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 const PORT = process.env.PORT || 4000;
 connectDB();
+
 
 
 app.use(cors())
@@ -27,6 +29,7 @@ app.get("/", (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 
 app.use(notFound);
