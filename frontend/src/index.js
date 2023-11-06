091cccd3bbcca4a0b198c228e71d3357eba714de
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-11-06 13:44:57 +0500

    shipping Sceen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,12 +13,14 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 
 const router=createBrowserRouter(
@@ -29,6 +31,10 @@ const router=createBrowserRouter(
       <Route  path='/cart' element={<CartScreen/>}/>
       <Route  path='/login' element={<LoginScreen/>}/>
       <Route  path='/register' element={<RegisterScreen/>}/>
+
+      <Route path='' element={<PrivateRoute/>}>
+        <Route  path='/shipping' element={<ShippingScreen/>}/>
+      </Route>
     </Route>
   )
 )
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -25,9 +25,12 @@ const CartScreen = () => {
   };
 
   const removeFromCartHandler = async (id) => {
-    dispatch (removeFromCart(id));
+    dispatch(removeFromCart(id));
   };
 
+  const checkoutHandler = () => {
+    navigate("/login?redirect=/shipping");
+  };
   return (
     <Row>
       <Col md={8}>
@@ -94,6 +97,7 @@ const CartScreen = () => {
                 type="button"
                 className="btn-block"
                 disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>
